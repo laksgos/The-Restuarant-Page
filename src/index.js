2d6fbc8a6260ad9f4e75b7f597ca1e7d57817f9f
Lakshay Gosain <laksgos@gmail.com>
2021-12-05 08:07:25 -0500

    Add gitignore, package.json, src and webpack

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//navigation Home, menu, contact  
+
+
+//Home
+// -description 
+// -hours 
+// -location 
+
+// Menu 
+// -drinks
+// -slides
+// -main 
+
+// Constact 
+// - three main people involved 
